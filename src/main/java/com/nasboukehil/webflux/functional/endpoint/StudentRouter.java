1424714efa32a6a06f4fe899d3e8bf3f3e799398
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-02-05 19:34:40 +0100

    implemented functionnal router

--- src/main/java/com/nasboukehil/webflux/functional/endpoint/StudentRouter.java
+++ src/main/java/com/nasboukehil/webflux/functional/endpoint/StudentRouter.java
@@ -1,5 +1,27 @@
 package com.nasboukehil.webflux.functional.endpoint;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
 public class StudentRouter {
 
+	@Autowired
+	private StudentHandler studentHandler;
+	
+	@Bean
+	RouterFunction<ServerResponse> returnStudent() {
+		return RouterFunctions.route(RequestPredicates.GET("/api/f/getStudent/{rollNo}"),
+															studentHandler::getStudent);
+	}
+	
+	RouterFunction<ServerResponse> returnAllStudents() {
+		return RouterFunctions.route(RequestPredicates.GET("/api/f/getAllStudents/"), 
+															studentHandler::getAllStudent);
+	}
 }
